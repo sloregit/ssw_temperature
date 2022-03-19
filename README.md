5f78f67aef1dbebd174665211db061c4476ea5f4
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-03-19 18:48:10 +0100

    3 files modified

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Visualizzare, oltre alla temperatura, anche la pressione
+Creare un tasto che calcoli la media della temperatura nelle città visualizzate
 
 Completare l'esercizio su stackblitz e nella consegna indicare il link
--- index.js
+++ index.js
@@ -3,39 +3,62 @@ import './style.css';
 
 //modificata api key con quella personale
 const apiKey = 'e6f38b0049fc0177522baf2baa00026f';
-//Es.1 aggiunta Lucca
+//oggetto request
 const leCitta = ['Genova', 'Milano', 'Torino', 'Roma', 'Lucca'];
 const URL =
   'https://api.openweathermap.org/data/2.5/weather?APPID=' +
   apiKey +
   '&units=metric&q=';
-// Crea una lista di bottoni con i nomi delle citta
+
+const risposta = document.getElementById('risposta');
+const buttonMedia = document.getElementById('media');
+buttonMedia.addEventListener('click', calcolaMediaTemp);
+
 leCitta.map((citta) => {
   const btn = document.createElement('button');
   btn.innerHTML = citta;
   btn.addEventListener('click', () => display(btn.innerHTML));
-
-  //Es.2 aggiunti callback per cambiare colore al pulsante
   btn.addEventListener('mouseenter', function () {
     this.style.backgroundColor = 'lightblue';
   });
   btn.addEventListener('mouseleave', function () {
     this.style.backgroundColor = 'inherit';
   });
-  //
   const item = document.createElement('li');
   item.appendChild(btn);
   document.getElementById('citta').appendChild(item);
 });
+var media = [];
+function calcolaMediaTemp() {
+  leCitta.map((città, i) => {
+    const tempRequest = new XMLHttpRequest();
+    tempRequest.onload = () => {
+      if (tempRequest.status === 200) {
+        var dataObject = JSON.parse(tempRequest.response);
+
+        return media.push(dataObject.main.temp);
+      } else {
+        console.log(tempRequest.status);
+        risposta.innerText = 'Errore';
+      }
+    };
+
+    tempRequest.open('GET', URL + città, true);
+    tempRequest.send();
+  });
+  risposta.innerHTML = media;
+}
+console.log(media);
+
 // Funzione collegata ai bottoni
 function display(c) {
-  const request = new XMLHttpRequest(); // Costruzione dell'oggetto "request"
+  const request = new XMLHttpRequest();
   // Funzione callback invocata quando la request termina
   request.onload = () => {
     // funzione definita arrow
     if (request.status === 200) {
       var dataObject = JSON.parse(request.response);
-      document.getElementById('risposta').innerHTML =
+      risposta.innerHTML =
         new Date().toISOString() +
         '</br>' +
         'A ' +
@@ -43,18 +66,15 @@ function display(c) {
         ' ci sono ' +
         dataObject.main.temp +
         ' gradi' +
-        //Es.3 Aggiunta la pressione (in hPa, Openweather)
         '</br>' +
         'La pressione è di: ' +
         dataObject.main.pressure +
         ' hPa';
     } else {
-      document.getElementById('risposta').innerText = 'Errore';
+      risposta.innerText = 'Errore';
     }
   };
-  // Applico il metodo "open"
   request.open('GET', URL + c, true);
-  // Applico il metodo send (al termine chiamera' il callback "onload")
   request.send();
   console.log(new Date().toISOString() + ': Finito');
 }
